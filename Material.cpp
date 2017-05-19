b78a1cda5f8e8f4cecb120f65f29389e0aba19ee
joanlojo <joanlojo.7@gmail.com>
2017-05-19 18:14:47 +0200

    correcion de gamma

--- Material.cpp
+++ Material.cpp
@@ -12,7 +12,8 @@ Material::Material(char *DiffPath, char*SpectPath, float Shini) {
 
 	int width, height;
 	unsigned char* image = SOIL_load_image(DiffPath, &width, &height, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -28,7 +29,8 @@ Material::Material(char *DiffPath, char*SpectPath, float Shini) {
 
 	int width2, height2;
 	unsigned char* image2 = SOIL_load_image(SpectPath, &width2, &height2, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image2);
 	glBindTexture(GL_TEXTURE_2D, 0);
